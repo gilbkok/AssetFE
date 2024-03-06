769aba174212b78f694497a29e786c8c2c167fa9
kokou <kpomenou@outlook.com>
2024-03-05 22:54:15 -0600

    first components

---
+++ assetmg/src/app/app.routes.ts
@@ -0,0 +1,9 @@
+import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ContainerComponent } from './container/container.component';
+import { HeaderComponent } from './container/header/header.component';
+import { MenuComponent } from './container/header/menu/menu.component';
+export const routes: Routes = [
+    
+];
---
+++ assetmg/src/app/container/header/menu/menu.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+
+
+@Component({
+  selector: 'app-menu',
+  standalone: true,
+  imports: [
+    MatCardModule
+  ],
+  templateUrl: './menu.component.html',
+  styleUrl: './menu.component.css'
+})
+export class MenuComponent {
+
+}
